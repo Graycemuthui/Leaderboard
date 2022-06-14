d954159019815c533669695bcca56d3cbe5334a7
Graycemuthui <graycemuthui20@gmail.com>
2022-06-14 16:49:17 +0300

    Create html structure

--- src/index.js
+++ src/index.js
@@ -1,13 +1,1 @@
-import _ from "lodash";
 import "./styles.css";
-
-function component() {
-  const element = document.createElement("div");
-
-  element.innerHTML = _.join(["Hello", "webpack"], " ");
-  element.classList.add("hello");
-
-  return element;
-}
-
-document.body.appendChild(component());
