758ccd89d6bec388ea906b0e264ce11466c6817b
Graycemuthui <graycemuthui20@gmail.com>
2022-06-16 18:16:13 +0300

    Style the body and sections

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-import './styles.css';
-import Api from './modules/api.js';
+import "./styles.css";
+import Api from "./modules/api.js";
+import party from "party-js";
 
-document.querySelector('.refresh-btn').addEventListener('click', Api.getScores);
+document.querySelector(".refresh-btn").addEventListener("click", Api.getScores);
 
 // Create an event listener for the submit button that will call the addScore function.
-document.querySelector('.add-input').addEventListener('submit', (e) => {
+document.querySelector(".add-input").addEventListener("submit", (e) => {
   e.preventDefault();
+  party.confetti(document.querySelector(".add-input"), {
+    count: party.variation.range(1000, 4000),
+  });
   Api.addScore();
 });
